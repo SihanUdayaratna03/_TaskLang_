ebecd9350ab1e624606009496afcb24176335945
Sihan Udayaratna <it24103532@my.sliit.lk>
2026-05-01 21:42:45 +0530

    Define the C structs for the AST in ast.h

---
+++ src/ast.h
@@ -0,0 +1,35 @@
+#ifndef AST_H
+#define AST_H
+
+typedef enum {
+    SCHED_DAILY,
+    SCHED_WEEKLY,
+    SCHED_TIMED,
+    SCHED_AFTER
+} ScheduleType;
+
+typedef struct {
+    ScheduleType type;
+    char *time;
+    char *weekday;
+    char *depends_on;
+} Schedule;
+
+typedef struct Task {
+    char *name;
+    char *script;
+    Schedule schedule;
+    int has_condition;
+    struct Task *next;
+} Task;
+
+typedef struct {
+    Task *head;
+    Task *tail;
+} TaskList;
+
+TaskList* create_task_list();
+void append_task(TaskList *list, Task *task);
+Task* create_task(char *name);
+
+#endif
